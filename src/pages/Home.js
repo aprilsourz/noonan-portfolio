8184ac4fb50c2905831393effe624d3c993050f0
Arlo Feirman <arlofeirman@gmail.com>
2020-04-13 08:07:58 -0600

    Add link to home button

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,14 @@
 import React from "react"
+import { Link } from "../components/typography"
 
 const Home = () => {
-  return <div>Home</div>
+  return (
+    <div>
+      Home
+      <br />
+      <Link to="style-guide"> Style Guide Page </Link>
+    </div>
+  )
 }
 
 export default Home
