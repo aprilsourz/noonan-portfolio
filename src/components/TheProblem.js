360535533d88c54d13ed1641189f1384fb01e4d0
Arlo Feirman <arlofeirman@gmail.com>
2020-07-19 11:10:26 -0600

    Refactor problem component

---
+++ src/components/TheProblem.js
@@ -0,0 +1,38 @@
+import breakpoint from "styled-components-breakpoint"
+import React from "react"
+import styled from "styled-components"
+
+import { ContentContainer, FullWidthBackground } from "./layout"
+import { HeaderMd, BodyText } from "./typography"
+
+const TheProblem = ({ blocksOfText, backgroundColor }) => (
+  <FullWidthBackground
+    className="responsive-align-center"
+    backgroundColor={backgroundColor || "lightBlue"}
+  >
+    <ContentContainer style={{ maxWidth: "1020px" }}>
+      <HeaderMdWithMargin>the problem</HeaderMdWithMargin>
+      {blocksOfText.map((blockOfText) => (
+        <BodyTextWithMargin>{blockOfText}</BodyTextWithMargin>
+      ))}
+    </ContentContainer>
+  </FullWidthBackground>
+)
+
+const BodyTextWithMargin = styled(BodyText)`
+  margin-bottom: 12px;
+
+  ${breakpoint("tablet")`
+    margin-bottom: 25px;
+  `}
+`
+
+const HeaderMdWithMargin = styled(HeaderMd)`
+  margin-bottom: 12px;
+
+  ${breakpoint("tablet")`
+    margin-bottom: 25px;
+  `}
+`
+
+export default TheProblem
