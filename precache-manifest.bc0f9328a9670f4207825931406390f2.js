66fde254b15eb7b920ca558d06fc4de33e7d460d
Arlo Feirman <arlofeirman@gmail.com>
2020-07-20 00:54:58 -0600

    Updates

--- precache-manifest.bc0f9328a9670f4207825931406390f2.js
+++ precache-manifest.bc0f9328a9670f4207825931406390f2.js
@@ -1,31 +1,103 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1c5ac621bc2be4d706aadca84ad5ad95",
+    "revision": "7e6752dca2ec106fc0202b2e81017c4e",
     "url": "/noonan-portfolio/index.html"
   },
   {
-    "revision": "931ce20a1961cea5cd54",
+    "revision": "fa069e783c53b9eb4a78",
     "url": "/noonan-portfolio/static/css/2.9e5c3921.chunk.css"
   },
   {
-    "revision": "92cd251481e43d2e5e49",
+    "revision": "94826eb1d3e21efc33d1",
     "url": "/noonan-portfolio/static/css/main.97d4e1c6.chunk.css"
   },
   {
-    "revision": "931ce20a1961cea5cd54",
-    "url": "/noonan-portfolio/static/js/2.e4704858.chunk.js"
+    "revision": "fa069e783c53b9eb4a78",
+    "url": "/noonan-portfolio/static/js/2.d9bec113.chunk.js"
   },
   {
     "revision": "3a5402f5ddd536a8f5deb8d906cf7b41",
-    "url": "/noonan-portfolio/static/js/2.e4704858.chunk.js.LICENSE.txt"
+    "url": "/noonan-portfolio/static/js/2.d9bec113.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "92cd251481e43d2e5e49",
-    "url": "/noonan-portfolio/static/js/main.100e681f.chunk.js"
+    "revision": "94826eb1d3e21efc33d1",
+    "url": "/noonan-portfolio/static/js/main.609511ea.chunk.js"
   },
   {
     "revision": "4f0ca1373c8810a22984",
     "url": "/noonan-portfolio/static/js/runtime-main.a2198669.js"
+  },
+  {
+    "revision": "3aa70fe41d9a8ab80801a7d959a600e8",
+    "url": "/noonan-portfolio/static/media/01-feelings.3aa70fe4.png"
+  },
+  {
+    "revision": "d54ce439d64a9ef4f2db6e48b23b4d05",
+    "url": "/noonan-portfolio/static/media/02-plants.d54ce439.png"
+  },
+  {
+    "revision": "2619d4c6f2eb33359f0d85dd887d3a30",
+    "url": "/noonan-portfolio/static/media/03-climbing1.2619d4c6.png"
+  },
+  {
+    "revision": "114fafd9d590fc00f90b68b7ba9659ac",
+    "url": "/noonan-portfolio/static/media/04-embroidery.114fafd9.png"
+  },
+  {
+    "revision": "a0510271c50907e1849fdc2500173166",
+    "url": "/noonan-portfolio/static/media/05-tatum.a0510271.png"
+  },
+  {
+    "revision": "a147fc5f95c494eccbc7d7b44dc0281e",
+    "url": "/noonan-portfolio/static/media/06-eggs.a147fc5f.png"
+  },
+  {
+    "revision": "fc9ce64137b450091bb688b2208f64aa",
+    "url": "/noonan-portfolio/static/media/07-mothmoon.fc9ce641.png"
+  },
+  {
+    "revision": "c7193b8a3c1feb19b6410179c5d84530",
+    "url": "/noonan-portfolio/static/media/08-snowboarding.c7193b8a.png"
+  },
+  {
+    "revision": "ceb52729c2eff4b9efe1437f6e53b2f1",
+    "url": "/noonan-portfolio/static/media/09-watercolor.ceb52729.png"
+  },
+  {
+    "revision": "e52f297819b9124323a0bb413fcc8894",
+    "url": "/noonan-portfolio/static/media/10-collagequeen.e52f2978.png"
+  },
+  {
+    "revision": "e5db3bd5fa16a56a681609088c3b11f4",
+    "url": "/noonan-portfolio/static/media/11-butterflies.e5db3bd5.png"
+  },
+  {
+    "revision": "e2dd61d51a9dd18bbceccd429a51d2f4",
+    "url": "/noonan-portfolio/static/media/12-climbing2.e2dd61d5.png"
+  },
+  {
+    "revision": "37d7ed594acdf2156742583465b4f302",
+    "url": "/noonan-portfolio/static/media/13-backcountry.37d7ed59.png"
+  },
+  {
+    "revision": "320c33c7c0e6f847a561c0df180b9f32",
+    "url": "/noonan-portfolio/static/media/14-weaving.320c33c7.png"
+  },
+  {
+    "revision": "109708a531bd4ea3aa80bc5d2657505c",
+    "url": "/noonan-portfolio/static/media/15-popsicle.109708a5.png"
+  },
+  {
+    "revision": "0d6669d27036295704ee105fff947087",
+    "url": "/noonan-portfolio/static/media/16-quilt.0d6669d2.png"
+  },
+  {
+    "revision": "75c13e6523211099f1a9b8ed5ab8de45",
+    "url": "/noonan-portfolio/static/media/17-biking.75c13e65.png"
+  },
+  {
+    "revision": "352ad4fb309059df7e6e2605a8c6318f",
+    "url": "/noonan-portfolio/static/media/18-collagelady.352ad4fb.png"
   },
   {
     "revision": "a7d12e631c2640ae47877144339e94cf",
@@ -40,8 +112,56 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/noonan-portfolio/static/media/carrienoonan.9a9b0143.svg"
   },
   {
+    "revision": "35b97121916cb13b9e6873c7e7b49afd",
+    "url": "/noonan-portfolio/static/media/chart-activity.35b97121.png"
+  },
+  {
+    "revision": "725b0be03cb0d34ae9cf5645c0f04bb2",
+    "url": "/noonan-portfolio/static/media/chart-time-activity.725b0be0.png"
+  },
+  {
+    "revision": "d6fe60855e06dd6534080d0e1b2b2136",
+    "url": "/noonan-portfolio/static/media/chart-time.d6fe6085.png"
+  },
+  {
     "revision": "f4594d58312c784d72f2a8881c26bb3c",
     "url": "/noonan-portfolio/static/media/clouds.f4594d58.svg"
+  },
+  {
+    "revision": "dfbb9aac267ea8c9b84e2946032d5146",
+    "url": "/noonan-portfolio/static/media/data-driven-decisions.dfbb9aac.png"
+  },
+  {
+    "revision": "603e70b51e112d858f73f18a21d38597",
+    "url": "/noonan-portfolio/static/media/data-driven_carousel1.603e70b5.png"
+  },
+  {
+    "revision": "34f7f48caeed48e2042911494e77bd05",
+    "url": "/noonan-portfolio/static/media/data-driven_carousel2.34f7f48c.png"
+  },
+  {
+    "revision": "f48c6d58603c4f45a5019dcfd4d3fb90",
+    "url": "/noonan-portfolio/static/media/data-driven_carousel3.f48c6d58.png"
+  },
+  {
+    "revision": "07698da6c96985293218044e41d515b4",
+    "url": "/noonan-portfolio/static/media/data-driven_carousel4.07698da6.png"
+  },
+  {
+    "revision": "85bb0d054d2c6518ed18f58e067414fb",
+    "url": "/noonan-portfolio/static/media/data-driven_carousel5.85bb0d05.png"
+  },
+  {
+    "revision": "90e52a43dcf224c2adf58fddb620ba43",
+    "url": "/noonan-portfolio/static/media/data-driven_carousel6.90e52a43.png"
+  },
+  {
+    "revision": "78ee3bf5ccc6c23cba4b9813de4b1ed8",
+    "url": "/noonan-portfolio/static/media/data-driven_carousel7.78ee3bf5.png"
+  },
+  {
+    "revision": "639673deae934a3b541e7b9c36cc85b7",
+    "url": "/noonan-portfolio/static/media/data-driven_carousel8.639673de.png"
   },
   {
     "revision": "e9cc527280608a3fd5c8975d31abcbef",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/noonan-portfolio/precache-manifest.8bcb9ea06ea81d69cb0158424b69e9f1.js"
+  "/noonan-portfolio/precache-manifest.bc0f9328a9670f4207825931406390f2.js"
 );
 
 self.addEventListener('message', (event) => {
