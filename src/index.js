6018058e026d5a5f6f555a6656cbc6a30afe1838
Arlo Feirman <arlofeirman@gmail.com>
2020-08-29 12:00:09 -0600

    Delete serviceWorker.js

--- src/index.js
+++ src/index.js
@@ -5,9 +5,6 @@ import App from "./App"
 import "./reset.css"
 import "./index.css"
 
-// @TODO Should we leave the service worker?
-import * as serviceWorker from "./serviceWorker"
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
@@ -15,7 +12,4 @@ ReactDOM.render(
   document.getElementById("root")
 )
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
+
